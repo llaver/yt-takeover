2815cbbd094096fec410222cb9e2618cca647a9c
rbell2 <ryan_bell@eogresources.com>
2019-07-01 17:32:02 -0500

    Start work on watch page description.

--- packages/ui-components/src/Watch/VideoDescription.js
+++ packages/ui-components/src/Watch/VideoDescription.js
@@ -3,19 +3,64 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
 import VideoCard from "../Common/VideoCard";
+import Typography from "@material-ui/core/Typography";
 
 const styles = {
-  container: {},
-  root: {}
+  container: {
+    height: '100%'
+  },
+  root: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  titleText: {
+    color: 'grey',
+    letterSpacing: '.3rem'
+  },
+  body: {
+  
+  },
+  bodyText: {
+    letterSpacing: '.1rem',
+    whiteSpace: 'pre-line'
+  },
+  continue: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  continueText: {
+    color: 'grey',
+    letterSpacing: '.3rem'
+  },
 };
 
 class VideoDescription extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, description } = this.props;
     return (
       <div className={classes.container}>
         <div className={classes.root}>
-          <div />
+          <div className={classes.title}>
+            <Typography className={classes.titleText} variant="h5">
+              Description
+            </Typography>
+          </div>
+          <div className={classes.body}>
+            <Typography className={classes.bodyText} variant="h6">
+              {description}
+            </Typography>
+          </div>
+          <div className={classes.continue}>
+            <Typography className={classes.continueText} variant="h5">
+              Continue ▼
+            </Typography>
+          </div>
         </div>
       </div>
     );
--- packages/ui-components/src/Watch/index.js
+++ packages/ui-components/src/Watch/index.js
@@ -20,18 +20,7 @@ const dummyVideoData = {
   likes: 69695,
   dislikes: 1075,
   length: "String/Object",
-};
-
-const dummyVideoData = {
-  id: 1,
-  linkId: "pl8cagt9gK0",
-  thumbnailUrl:
-    "https://i.ytimg.com/vi/pl8cagt9gK0",
-  title: "Pewdiepie vs. T-Series",
-  creator: "DJ Khaled",
-  creatorId: "DjKhaled",
-  views: 17109067,
-  likes: 69695,
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \nLorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \nIt was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 };
 
 const dummyPlayerData = {
@@ -110,7 +99,6 @@ const styles = {
     minHeight: "20vh"
   },
   videoDescription: {
-    backgroundColor: "grey",
     height: "100%",
     flex: 2,
     minWidth: "250px"
@@ -131,7 +119,7 @@ class Watch extends Component {
               <VideoInfo />
             </div>
             <div className={classes.videoDescription}>
-              <VideoDescription />
+              <VideoDescription description={dummyVideoData.description} />
             </div>
           </div>
           <div className={classes.watchNext}>
